f02c0dc7833f80be16e13183f61ad9a6f3bf5865
Andrew <andrewurom@gmail.com>
2021-04-24 02:04:38 +0100

    Implemented the delete button to delete todo

--- simple-todo/src/components/TodoList.jsx
+++ simple-todo/src/components/TodoList.jsx
@@ -4,14 +4,14 @@ import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
 import Todo from "./Todo";
 
-function TodoList({ todos }) {
+function TodoList({ todos, removeTodo }) {
   console.log(todos);
   return (
     <Paper>
       <List>
         {todos.map((todo) => (
           <>
-            <Todo task = {todo.task} key = {todo.id} completed = {todo.completed}/>
+            <Todo id = {todo.id} task = {todo.task} key = {todo.id} completed = {todo.completed} removeTodo={removeTodo}/>
             <Divider />
           </>
         ))}
--- simple-todo/src/views/TodoApp.jsx
+++ simple-todo/src/views/TodoApp.jsx
@@ -19,6 +19,11 @@ function TodoApp() {
   const addTodo = (newTodoText) => {
     setTodos([...todos, { id: 4, task: newTodoText, completed: false }]);
   };
+    const removeTodo = todoId => {
+        let updatedTodos = todos.filter(todo => todo.id !== todoId)
+
+        setTodos(updatedTodos)
+    }
 
   return (
     <Paper
@@ -38,7 +43,7 @@ function TodoApp() {
       <Grid container justify='center' style={{marginTop: "1rem"}}>
         <Grid item xs={11} md={8} lg={8}>
           <TodoForm addTodo={addTodo} />
-          <TodoList todos={todos} />
+          <TodoList todos={todos} removeTodo={removeTodo} />
         </Grid>
       </Grid>
     </Paper>
